7921d06f32d59517ceb8cf59549f88abc75ff169
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-06-06 01:30:35 +0700

    Update 12.1

--- EGE/12.1.py
+++ EGE/12.1.py
@@ -22,3 +22,37 @@ def f(str: str) -> str:
 
 s = '8' * 45
 print(f(s))
+
+
+def sum_nums(number: int) -> int:
+    result = 0
+
+    while number > 0:
+        result += number % 10
+        number //= 10
+
+    return result
+
+
+# Второй случай
+def func(s: str) -> str:
+    while '25' in s or '355' in s or '555' in s:
+
+        if '25' in s:
+            s = s.replace('25', '3', 1)
+        if '355' in s:
+            s = s.replace('355', '52', 1)
+        if '555' in s:
+            s = s.replace('555', '23', 1)
+
+    return s
+
+
+s = ''
+for n in range(4, 1000):
+    s = '3' + '5' * n
+    c = func(s)
+
+    if sum_nums(int(c)) == 27:
+        print(n)
+        break
