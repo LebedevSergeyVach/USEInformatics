174f6c9a0ac9a8e65398066e1bfa9e4453294251
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-06-06 16:53:12 +0700

    Update 25.1

--- EGE/25.1.py
+++ EGE/25.1.py
@@ -1,3 +1,22 @@
+from fnmatch import fnmatch
+
+mask = '5?2*3?3?'
+div = 98591
+
+# Считаем делитель для маски '?2*3?3?' => 10 ** 6
+div_i = 0
+for i in range(10 ** 6, 10 ** 10 + 1):
+    if i % div == 0:
+        div_i = i
+        # print(div_i) == 1084501
+        break
+
+# Выводим все числа, относящиеся к маске
+for i in range(1084501, 10 ** 10 + 1, div):
+    if fnmatch(str(i), mask) and i % div == 0:
+        print(i, i // div)
+
+
 # Маска числа
 from fnmatch import fnmatch
 
@@ -78,3 +97,20 @@ for i in range(1, 10**9 + 1):
 for i in range(1, 10**9 + 1):
     if fnmatch(str(i), mask) and i % div_one == 0 and i % div_two == 0:
         print(i, count_div(i))
+
+# ДВЕ МАСКИ
+from fnmatch import fnmatch
+
+
+mask_one = '4*5*6' # 10 ** 3
+mask_two = '?74*68?' # 10 ** 5
+div = 1234
+
+for i in range(10 ** 5, 10 ** 10 + 1):
+    if i % div == 0:
+        print(i) # 101188
+        break
+
+for i in range(101188, 10 ** 10 + 1, div):
+    if fnmatch(str(i), mask_one) and fnmatch(str(i), mask_two) and i % div == 0:
+        print(i, i // div)
